6fe7bfd0b513c554dbdd5b65b8b41d69755b8ee5
alishfikkarun <163851277+alishfikkarun@users.noreply.github.com>
2025-08-12 09:16:01 +0500

    Update main.jsx

--- webapp/src/main.jsx
+++ webapp/src/main.jsx
@@ -1,1 +1,18 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import GiftPage from "./pages/GiftPage";
+import "./index.css";
 
+function App(){
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/gift/:slug" element={<GiftPage/>} />
+        <Route path="*" element={<div className="min-h-screen bg-[#0d1117] text-white flex items-center justify-center">Open a gift link</div>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+createRoot(document.getElementById("root")).render(<App/>);
